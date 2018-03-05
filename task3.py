ddd6e397cbf883bde5fdae11dccbc05e0efd877c
Yauhen Khodzin <yauhen_khodzin@epam.com>
2018-03-05 13:40:00 +0000

    task3 fix & task4

--- task3.py
+++ task3.py
@@ -3,8 +3,4 @@
 # pylint: disable=invalid-name
 x = int(input("Input X: "))
 y = int(input("Input Y: "))
-print(x)
-print(y)
-print(bin(x ^ y)[2:])
-print(len(bin(x ^ y)[2:]))
 print((bin(x ^ y)[2:]).count('1'))
--- task4.py
+++ task4.py
@@ -1,5 +1,24 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+'''Task4'''
+# pylint: disable=invalid-name
+import re
 N = int(input("Input N: "))
-while i<N
-   
-y = int(input("Input Y: "))
+print(N)
+i = 0
+mylist = []
+while i < N:
+    mylist.append(input("Input string: "))
+    i += 1
+result = -1
+flag = 0
+temp = 0
+for x in range(N):
+    if int(mylist[x][-1]) == 1:
+        if flag == 0:
+            temp = int(re.search(r'\d+', mylist[x]).group())
+            result = x + 1
+            flag = 1
+        if temp < int(re.search(r'\d+', mylist[x]).group()):
+            temp = int(re.search(r'\d+', mylist[x]).group())
+            result = x + 1
+print(result)
