fa3f86a1bd3749193446e8fdb5e1a3eaece7a880
Yauhen Khodzin <yauhen_khodzin@epam.com>
2018-03-12 06:26:13 +0000

    flag fix

--- task4.py
+++ task4.py
@@ -10,14 +10,9 @@ while i < N:
     mylist.append(input("Input string: "))
     i += 1
 result = -1
-flag = 0
-temp = 0
+temp = -1
 for x in range(N):
     if int(mylist[x][-1]) == 1:
-        if flag == 0:
-            temp = int(re.search(r'\d+', mylist[x]).group())
-            result = x + 1
-            flag = 1
         if temp < int(re.search(r'\d+', mylist[x]).group()):
             temp = int(re.search(r'\d+', mylist[x]).group())
             result = x + 1
